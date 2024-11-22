9918777e4bad1f83b7733fda37decd04626bed0c
HarhsnaZkewed <150878367+HarhsnaZkewed@users.noreply.github.com>
2024-11-22 10:33:00 +0530

    update.gitignore3

--- harigana_land/settings.py
+++ harigana_land/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-z7e#%ipgwl5f6i3gbn%wc!ldcjtn3bpj^&%rdd#(&8lb2u#$yg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
